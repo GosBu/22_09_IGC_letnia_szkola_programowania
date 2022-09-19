6494244be99579a4a42d18fdefb4ac6397cd17a1
Gosia Bujak <magorzata.bujak@gmai.com>
2022-09-19 17:35:38 +0200

    Initial commit

--- zadanie_TXT4.py
+++
@@ -1,6 +0,0 @@
-# TXT4 Podziel tekst wprowadzony przez użytkownika na poszczególne wyrazy,
-# lub na poszczególne zdania
-
-tekst = input('Wprowadź tekst: ')
-print(tekst.split())
-print(tekst.split('.'))
