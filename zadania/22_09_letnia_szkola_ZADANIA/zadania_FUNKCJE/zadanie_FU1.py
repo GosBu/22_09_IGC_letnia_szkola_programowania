a0ecebd8fd1292a569ff6d3fc1d4c29adb656b37
Gosia Bujak <magorzata.bujak@gmai.com>
2022-09-21 14:02:48 +0200

    Initial commit

---
+++ zadania/22_09_letnia_szkola_ZADANIA/zadania_FUNKCJE/zadanie_FU1.py
@@ -0,0 +1,7 @@
+# FU1 Stwórz funkcję, która będzie przyjmowała wartości imię nazwisko i wiek,
+# gdzie wiek będzie miał wartość domyślną 3 i wypisze je na ekranie
+# 1) wywołaj funkcję z podaniem i bez podania wieku
+# 2) przekaż funkcji wartości ze słownika
+# {'imie': 'Waldemar', 'nazwisko': 'Nowak', 'wiek': 23}
+# podczas wywołania
+
